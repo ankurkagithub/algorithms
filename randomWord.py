7f4c2d0ad8327776eb868b4759f9180490305f37
Ankur Kaushal <ankaushal@gmail.com>
2023-01-26 12:10:45 +0000

    randomWord file update

---
+++ randomWord.py
@@ -0,0 +1,22 @@
+# Using Knuth's method
+# More details on method here - https://www.youtube.com/watch?v=i8kD33wx9Mo&ab_channel=JamesCutajar
+
+import sys
+
+def get_randomWord():
+    i = len(sys.argv)
+
+    if i < 0:
+        return champion
+    else:
+        for j in range(1, i):
+            
+
+
+
+def main():
+    get_randomWord()
+
+
+if __name__ == "__main__":
+    main()
