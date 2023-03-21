f262230bb461fc8ebc77594f603a53fd8d964976
Ankur <105734576+ankurkagithub@users.noreply.github.com>
2023-03-21 11:14:56 +0000

    added recursion to add nums

--- sum_natural_numbers.py
+++ sum_natural_numbers.py
@@ -1,8 +1,23 @@
+#Doing sum using iterative approach
+
 def sum_NN(n):
     sum = 0
     for i in range(n+1):
         sum += i
     return sum
 
-print(sum_NN(5))
-print("Git is awesome")
+
+# Doing sum using recursive approach
+def sum_list(N):
+    # Base case
+    if len(N) == 0:
+        return 0
+    # recursion + calculation
+    return N[0] + sum_list(N[1:])
+
+# List comprehension
+alist = [num for num in range(1,10)]
+
+# Using print functions.
+print(sum_list(alist))
+print(sum_NN(9))
